91a0216e34b6b1bd0584fffccf82fc8e6721ebb4
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2025-05-27 23:02:28 -0600

    :sunglasses: Versión estable 1.7 | Soporte de temas de aplicación y correo electrónico

--- app/src/main/java/com/app/tibibalance/ui/screens/auth/signup/SignUpScreen.kt
+++ app/src/main/java/com/app/tibibalance/ui/screens/auth/signup/SignUpScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
